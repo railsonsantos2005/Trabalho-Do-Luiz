4215d9f9a6d1b2fa91654caf4b5ae03e888c848b
Railson <railsonsantos2005@gmail.com>
2025-02-28 20:00:15 -0300

    Questão01

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -1,5 +1,18 @@
-import 'dart:io';
+Import 'dart:io';
+
+void cadastrarCategoria() {
+  List<String> categorias = ["eletronicos", "alimentos", "vestuario", "livros"];
+
+  stdout.write("Digite a categoria do produto (eletronicos, alimentos, vestuario, livros): ");
+  String? categoria = stdin.readLineSync()?.trim().toLowerCase();
+
+  if (categoria != null && categorias.contains(categoria)) {
+    print("Categoria válida: $categoria");
+  } else {
+    print("Categoria inválida. As opções válidas são: ${categorias.join(", ")}");
+  }
+}
 
 void main() {
-
+  cadastrarCategoria();
 }
